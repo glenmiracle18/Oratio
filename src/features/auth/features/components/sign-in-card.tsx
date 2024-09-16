6bf6453e59047ce4cf6e779fd19f619c7f94b94f
glenmiracle18 <m.bonyu@alustudent.com>
2024-09-16 18:04:08 +0200

    build: user information

--- src/features/auth/features/components/sign-in-card.tsx
+++ src/features/auth/features/components/sign-in-card.tsx
@@ -11,6 +11,7 @@ import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa6";
+import { TriangleAlert } from "lucide-react";
 import type { SignInFlow } from "../auth/types";
 import { useState } from "react";
 import { useAuthActions } from "@convex-dev/auth/react";
@@ -23,10 +24,28 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
   const { signIn } = useAuthActions();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState<string>("");
 
-  const hanldeProvider = (value: "github" | "gooogle") => {
-    signIn(value);
+  const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setPending(true);
+    signIn("password", { email, password, flow: "signIn" })
+      .catch(() => {
+        setError("Invalid email or password");
+      })
+      .finally(() => {
+        setPending(false);
+      });
   };
+
+  const hanldeProvider = (value: "github" | "google") => {
+    setPending(true);
+    signIn(value).finally(() => {
+      setPending(false);
+    });
+  };
+
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -36,15 +55,21 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md mb-6 flex items-center gap-x-2 text-sm text-destructive">
+          <TriangleAlert />
+          <p>{error}</p>
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form onSubmit={onPasswordSignIn} className="space-y-2.5">
           <Input
             type="email"
             placeholder="Email"
             required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            disabled={false}
+            disabled={pending}
           />
           <Input
             type="password"
@@ -52,27 +77,31 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
             required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            disabled={false}
+            disabled={pending}
           />
-          <Button type="submit" className="w-full " size="lg" disabled={false}>
+          <Button
+            type="submit"
+            className="w-full "
+            size="lg"
+            disabled={pending}
+          >
             Continue
           </Button>
         </form>
         <Separator />
-
         <div className="flex flex-col gap-y-2.5">
           <Button
-            disabled={false}
+            disabled={pending}
             className="w-full relative"
             variant="outline"
-            onClick={() => {}}
+            onClick={() => hanldeProvider("google")}
             size="lg"
           >
             Continue with Google
             <FcGoogle className="absolute size-5 top-2.5 left-2.5" />
           </Button>
           <Button
-            disabled={false}
+            disabled={pending}
             className="w-full relative"
             variant="outline"
             onClick={() => hanldeProvider("github")}
--- src/features/auth/features/components/sign-up-card.tsx
+++ src/features/auth/features/components/sign-up-card.tsx
@@ -10,6 +10,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { FcGoogle } from "react-icons/fc";
+import { TriangleAlert } from "lucide-react";
 import { FaGithub } from "react-icons/fa6";
 import type { SignInFlow } from "../auth/types";
 import { useState } from "react";
@@ -22,8 +23,37 @@ interface SignUpCardProps {
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
   const { signIn } = useAuthActions();
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState<string>("");
+
+  const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match!");
+      return;
+    }
+
+    setPending(true);
+    signIn("password", { email, name, password, flow: "signUp" })
+      .catch(() => {
+        setError("Something went wrong. Please try again.");
+      })
+      .finally(() => {
+        setPending(false);
+      });
+  };
+
+  const handleProvider = (value: "github" | "google") => {
+    setPending(true);
+    signIn(value).finally(() => {
+      setPending(false);
+    });
+  };
+
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -33,15 +63,28 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md mb-6 flex items-center gap-x-2 text-sm text-destructive">
+          <TriangleAlert />
+          <p>{error}</p>
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form onSubmit={onPasswordSignUp} className="space-y-2.5">
+          <Input
+            placeholder="FullName"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={pending}
+          />
           <Input
             type="email"
             placeholder="Email"
             required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            disabled={false}
+            disabled={pending}
           />
           <Input
             type="password"
@@ -49,7 +92,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            disabled={false}
+            disabled={pending}
           />
           <Input
             type="password"
@@ -57,9 +100,14 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             required
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-            disabled={false}
+            disabled={pending}
           />
-          <Button type="submit" className="w-full " size="lg" disabled={false}>
+          <Button
+            type="submit"
+            className="w-full "
+            size="lg"
+            disabled={pending}
+          >
             Continue
           </Button>
         </form>
@@ -67,20 +115,20 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
 
         <div className="flex flex-col gap-y-2.5">
           <Button
-            disabled={false}
+            disabled={pending}
             className="w-full relative"
             variant="outline"
-            onClick={() => {}}
+            onClick={() => handleProvider("google")}
             size="lg"
           >
             Continue with Google
             <FcGoogle className="absolute size-5 top-2.5 left-2.5" />
           </Button>
           <Button
-            disabled={false}
+            disabled={pending}
             className="w-full relative"
             variant="outline"
-            onClick={() => void signIn("github")}
+            onClick={() => handleProvider("github")}
             size="lg"
           >
             Continue with Github
